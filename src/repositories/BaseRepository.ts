ed18f7806738beb64a8a8cf4b04101d97956d3bb
WillyMilimo <milicodemoonga@gmail.com>
2021-09-08 19:54:39 +0200

    update done

--- src/repositories/BaseRepository.ts
+++ src/repositories/BaseRepository.ts
@@ -1,38 +1,140 @@
 /* eslint-disable no-unused-vars */
 
-import {Pool} from 'pg'
+import {Pool, QueryResult} from 'pg'
 import {pool} from '../loaders/database'
 
 export interface RepositoryI<T = unknown> {
-  insert(model: T): boolean
+  insert(model: T): Promise<QueryResult<T>>
 
-  insertMany(models: T[]): boolean
+  update(id: number, model: T): Promise<T | false>
 
-  findById(id: number): T
+  insertMany(models: T[]): Promise<boolean>
 
-  findAll(): T[]
+  findById(id: number): Promise<T | null>
 
-  deleteById(id: number): boolean
+  findAll(): Promise<T[]>
 
-  find(filter: Record<string, string | number>): T[]
+  deleteById(id: number): Promise<boolean>
+
+  find(filter: Record<string, string | number>): Promise<T[]>
 }
 
 export abstract class BaseRepository<T = unknown> implements RepositoryI<T> {
   protected pool: Pool
+  protected tableName: string
+  protected columns: string[]
+  protected idColumn: string
 
-  constructor() {
+  constructor({
+    tableName,
+    columns,
+    idColumn,
+  }: {
+    idColumn?: string
+    tableName: string
+    columns: string[]
+  }) {
     this.pool = pool
+    this.idColumn = idColumn || 'id'
+    this.columns = columns
+    this.tableName = tableName
   }
 
-  public abstract insert(model: T): boolean
+  public insert(model: T): Promise<QueryResult<T>> {
+    const {columns, placeholders, values} = this.generateInsertQueryParts(model)
 
-  public abstract insertMany(models: T[]): boolean
+    const query = `INSERT INTO ${this.tableName} (${columns}) VALUES (${placeholders}) RETURNING *`
 
-  public abstract findById(id: number): T
+    return this.pool.query(query, values)
+  }
 
-  public abstract findAll(): T[]
+  public async update(id: number, model: Partial<T>): Promise<T | false> {
+    const item = await this.findById(id)
 
-  public abstract deleteById(id: number): boolean
+    if (item) {
+      const {columns, values} = this.generateUpdateQueryParts(model)
+      const query = `UPDATE ${this.tableName} SET (${columns}) WHERE ${
+        this.idColumn
+      } = $${values.length + 1} RETURNING *`
 
-  public abstract find(filter: Record<string, string | number>): T[]
+      const {rows} = await this.pool.query<T>(query, [...values, id])
+
+      if (rows.length) return rows[0]
+    }
+
+    return false
+  }
+
+  public abstract insertMany(models: T[]): Promise<boolean>
+
+  public async findById(id: number): Promise<T | null> {
+    const columns = this.getColumns()
+    const {rows} = await this.pool.query<T>(
+      `SELECT ${columns} FROM ${this.tableName} WHERE ${this.idColumn} = ?`,
+      [id],
+    )
+
+    if (rows.length) return rows[0]
+
+    return null
+  }
+
+  public abstract findAll(): Promise<T[]>
+
+  public abstract deleteById(id: number): Promise<boolean>
+
+  public abstract find(filter: Record<string, string | number>): Promise<T[]>
+
+  private generateInsertQueryParts(model: Partial<T>) {
+    const cols = this.columns.filter(
+      col => col !== this.idColumn || !/atedAt$/g.test(col),
+    ) // remove id column and date guys
+    return cols.reduce(
+      (acc, col, index) => {
+        // eslint-disable-next-line no-param-reassign
+        acc.columns += index === cols.length - 1 ? `\`${col}\`` : `\`${col}\`, `
+
+        const param = index + 1
+        // eslint-disable-next-line no-param-reassign
+        acc.placeholders +=
+          index === cols.length - 1 ? `$${param}` : `$${param}, `
+
+        // eslint-disable-next-line no-param-reassign
+        acc.values.push(model[col])
+
+        return acc
+      },
+      {columns: '', placeholders: '', values: []},
+    )
+  }
+
+  private generateUpdateQueryParts(model: Partial<T>) {
+    const cols = Object.keys(model).filter(
+      col => col !== this.idColumn || !/atedAt$/g.test(col),
+    ) // remove id column and date guys
+    return cols.reduce(
+      (acc, col, index) => {
+        const param = index + 1
+        // eslint-disable-next-line no-param-reassign
+        acc.columns +=
+          index === cols.length - 1
+            ? `\`${col}\` = $${param}`
+            : `\`${col}\` = $${param}, `
+
+        acc.values.push(model[col])
+
+        return acc
+      },
+      {columns: '', values: []},
+    )
+  }
+
+  private getColumns(): string {
+    return this.columns.reduce((acc, col, index) => {
+      // eslint-disable-next-line no-param-reassign
+      acc += index === this.columns.length - 1 ? `\`${col}\`` : `\`${col}\`, `
+
+      return acc
+    }, '')
+  }
 }
--- src/repositories/UserRepository.ts
+++ src/repositories/UserRepository.ts
@@ -1,4 +1,18 @@
-import { IUser } from '../models/user';
+import {IUser} from '../models/user'
 import {BaseRepository, RepositoryI} from './BaseRepository'
 
-class UserRepository extends BaseRepository<IUser> implements RepositoryI {}
+class UserRepository extends BaseRepository<IUser> implements RepositoryI {
+  public insert(model: T): Promise<boolean> {
+    return this.pool.query("INSERT INTO")
+  }
+
+  public abstract insertMany(models: T[]): boolean
+
+  public abstract findById(id: number): T
+
+  public abstract findAll(): T[]
+
+  public abstract deleteById(id: number): boolean
+
+  public abstract find(filter: Record<string, string | number>): T[]
+}
