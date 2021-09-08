85975dbbc9e3198eb3fa302d7041e95235789500
WillyMilimo <milicodemoonga@gmail.com>
2021-09-08 05:51:14 +0200

    initial setup

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb',
+    'prettier',
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 12,
+    'sourceType': 'module'
+  },
+  'plugins': [
+    '@typescript-eslint'
+  ],
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+  'rules': {
+    'no-unneeded-ternary': 'off',
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ]
+  }
+}
---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  bracketSpacing: false,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+  semi: false,
+  tadWidth: 2,
+  tabs: true,
+};
