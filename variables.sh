b21544f61a872eb242e00c6d926908b31f1bcf0f
WillyMilimo <milicodemoonga@gmail.com>
2021-10-13 14:07:28 +0200

    added environments

---
+++ variables.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export pgConnectionString=postgres://nctdoqvbfwdsfo:ce79098213f552d87731f23befe05e26867a519c1cad671b849f81f7723c996d@ec2-54-90-211-192.compute-1.amazonaws.com:5432/dd8hk4b1uh70sh
+export host=ec2-54-90-211-192.compute-1.amazonaws.com
+export database=dd8hk4b1uh70sh
+export user=nctdoqvbfwdsfo
+export password=ce79098213f552d87731f23befe05e26867a519c1cad671b849f81f7723c996d
+export port=5432
+export API_VERSION=v1.0
+
+export PORT=9000
+
+export passphrase='g4+[!(G]NNK_akGkEj/)8gPJy*g);fDfY=kh~`fq/NgQk`>9)3^MVBaEUQt~\`y-'
+export jwtExpiry='90d'
+
+export emailRegex='^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$'
+
+
+export GOOGLE_APPLICATION_CREDENTIALS=/Users/willymilimo/Workspace/pcl-api/keys/pcl-service.json
