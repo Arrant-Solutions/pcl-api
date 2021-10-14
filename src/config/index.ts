7bb9326332a290b6f6ffad875bc30a32878dcdc4
WillyMilimo <milicodemoonga@gmail.com>
2021-10-14 07:31:13 +0200

    trashed keys

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,30 +1,28 @@
-import {readFileSync} from 'fs'
+// import {readFileSync} from 'fs'
 
 export const database = {
   port: 9000,
-  dbURL:
-    process.env.pgConnectionString ||
-    'pgConnectionString=postgres://nctdoqvbfwdsfo:ce79098213f552d87731f23befe05e26867a519c1cad671b849f81f7723c996d@ec2-54-90-211-192.compute-1.amazonaws.com:5432/dd8hk4b1uh70sh',
-  name: process.env.database || 'dd8hk4b1uh70sh',
+  dbURL: process.env.DATABASE_URL,
+  name: process.env.DATABASE_NAME,
 }
 
 export const PORT = process.env.PORT || 9000
 
-export const privateKey = readFileSync(
-  `${__dirname}/../keys/private-key.pem`,
-  'utf8',
-)
+// export const privateKey = readFileSync(
+//   `${__dirname}/../keys/private-key.pem`,
+//   'utf8',
+// )
 
-export const publicKey = readFileSync(
-  `${__dirname}/../keys/public-key.pem`,
-  'utf8',
-)
+// export const publicKey = readFileSync(
+//   `${__dirname}/../keys/public-key.pem`,
+//   'utf8',
+// )
 
-export const jwtSecret =
-  process.env.passphrase ||
-  'g4+[!(G]NNK_akGkEj/)8gPJy*g);fDfY=kh~`fq/NgQk`>9)3^MVBaEUQt~`y-'
+export const {PRIVATE_KEY, PUBLIC_KEY, JWT_SECRET, JWT_EXPIRY} = process.env
 
-export const jwtExpiry = process.env.jwtExpiry || '90d'
+// export const jwtSecret = process.env.passphrase
+
+// export const jwtExpiry = process.env.jwtExpiry || '90d'
 
 export enum Errors {
   // eslint-disable-next-line no-unused-vars
@@ -32,7 +30,6 @@ export enum Errors {
 }
 
 export const emailRegex =
-  process.env.emailRegex ||
   '^(([^<>()[].,;:s@"]+(.[^<>()[].,;:s@"]+)*)|(".+"))@(([^<>()[].,;:s@"]+.)+[^<>()[].,;:s@"]{2,})$'
 
 export const API_VERSION = process.env.API_VERSION || 'v1.0'
--- variables.sh
+++ variables.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 
-export pgConnectionString=postgres://nctdoqvbfwdsfo:ce79098213f552d87731f23befe05e26867a519c1cad671b849f81f7723c996d@ec2-54-90-211-192.compute-1.amazonaws.com:5432/dd8hk4b1uh70sh
-export host=ec2-54-90-211-192.compute-1.amazonaws.com
-export database=dd8hk4b1uh70sh
-export user=nctdoqvbfwdsfo
-export password=ce79098213f552d87731f23befe05e26867a519c1cad671b849f81f7723c996d
-export port=5432
-export API_VERSION=v1.0
-export ENV=production
-export env=production
-export environment=production
+heroku config:set DATABASE_URL=postgres://nctdoqvbfwdsfo:ce79098213f552d87731f23befe05e26867a519c1cad671b849f81f7723c996d@ec2-54-90-211-192.compute-1.amazonaws.com:5432/dd8hk4b1uh70sh
+heroku config:set HOST=ec2-54-90-211-192.compute-1.amazonaws.com
+heroku config:set DATABASE_NAME=dd8hk4b1uh70sh
+heroku config:set USER=nctdoqvbfwdsfo
+heroku config:set PASSWORD=ce79098213f552d87731f23befe05e26867a519c1cad671b849f81f7723c996d
+heroku config:set PG_PORT=5432
+heroku config:set API_VERSION=v1.0
+heroku config:set ENV=production
+heroku config:set env=production
+heroku config:set environment=production
 
-export PORT=9000
+heroku config:set PORT=9000
 
-export passphrase='g4+[!(G]NNK_akGkEj/)8gPJy*g);fDfY=kh~`fq/NgQk`>9)3^MVBaEUQt~\`y-'
-export jwtExpiry='90d'
+heroku config:set JWT_SECRET_KEY='g4+[!(G]NNK_akGkEj/)8gPJy*g);fDfY=kh~`fq/NgQk`>9)3^MVBaEUQt~\`y-'
+heroku config:set JWT_EXPIRY='90d'
 
-export emailRegex='^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$'
+heroku config:set PUBLIC_KEY=`cat keys/public-key.pem` 
 
-
-export GOOGLE_APPLICATION_CREDENTIALS=/Users/willymilimo/Workspace/pcl-api/keys/pcl-service.json
-
-echo "done running ash"
+heroku config:set GOOGLE_APPLICATION_CREDENTIALS=/Users/willymilimo/Workspace/pcl-api/keys/pcl-service.json
